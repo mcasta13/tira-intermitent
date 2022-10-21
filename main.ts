4dd65c67918b5adcfd51ebe206f6301eeaa9927c
mcasta13 <116083045+mcasta13@users.noreply.github.com>
2022-10-21 19:07:33 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        for (let index = 0; index < 8; index++) {
+    if (input.buttonIsPressed(Button.B)) {
+        for (let index = 0; index < 4; index++) {
             for (let index = 0; index <= 4; index++) {
                 led.toggle(index, 2)
             }
             basic.pause(1000)
         }
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        for (let index = 0; index <= 4; index++) {
+            basic.pause(1000)
+            led.toggle(index, 2)
+        }
+        basic.pause(1000)
     }
 })
